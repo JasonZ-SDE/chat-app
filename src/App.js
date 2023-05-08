90c408a3c8de7f64ef2c34518637d79f4cfa6c32
Jason Zheng <hanguang.sde@gmail.com>
2023-05-08 08:12:31 -0700

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+
 import "./App.css";
 
 function App() {
-  return <div>Hello World!</div>;
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <div>
+      <ChatEngine
+        height="100vh"
+        projectID="139732d9-2744-48fc-a102-f280362d18c9"
+        userName="Jason"
+        userSecret="qwerty"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() =>
+          new Audio("https://chat-engine-assets.s3.amazonaws.com/click.mp3")
+        }
+      />
+    </div>
+  );
 }
 
 export default App;
